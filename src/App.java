de1176013348c485e1b1fb6fa6b1c125383218e9
Jaakko Kulmala <jaakko.kulmala@student.hamk.fi>
2024-09-18 19:10:20 +0300

    Tehty alustus muistipelille

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 
+
 public class App {
     public static void main(String[] args) {
 
@@ -10,25 +11,25 @@ public class App {
         int [] numerot = new int[7];
         
 
+        //arvo 7 numeroa väliltä 1-5
         for (int i = 0; i < numerot.length; i++){
 
             numerot[i] = arvonta.nextInt(5) + 1;
             
-        }
+        } //printtaa viesti arvottujen nr jälkeen
         System.out.println("Yritä mnuistaa alla olevat numerot. Näet numerot 3 sekunnin ajan.");
-          for (int num : numerot)
+          //väli jokaisen numeron jälkeen
+        for (int num : numerot)
           
           {
             System.out.print(num + " ");
         }
-            
-            
-           
-        
-
+        System.out.println();
        
 
-       
+       }
+
     }
-}
+    
+
 
